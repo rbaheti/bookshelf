80838c76f1b2ea1ea1a37912938db34a1fc50df9
Rashmi <baheti.rash123@gmail.com>
2019-04-06 15:24:08 -0400

    fix: Set book-shelf default dropdown choice to its correct choice

--- src/Book.js
+++ src/Book.js
@@ -7,18 +7,19 @@ class Book extends Component {
 
   render() {
     const {book} = this.props;
+    const optionsState = book.shelf;
 
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
-            <img src={book.imageLinks.thumbnail} style={{visibility: "hidden"}}/>
+          <div className="book-cover" style={{backgroundSize: "contain", backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}>
+            <img src={book.imageLinks.thumbnail} style={{maxWidth: "150px", maxHeight: "200px", visibility: "hidden"}}/>
           </div>
           <div className="book-shelf-changer">
-            <select>
+            <select defaultValue={optionsState}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
+              <option value="wantToRead" defaultValue="wantToRead">Want to Read</option>
               <option value="read">Read</option>
               <option value="none">None</option>
             </select>
